01609c339f657cf46b4905506660220561b3ac1b
MD SAHI ALAM KHAN <ksahi4088@gmail.com>
2022-07-08 18:41:50 +0530

    footer section update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 
+// Banner Slider
 $('.banner__container').slick({
     dots: true,
     infinite: true,
@@ -9,39 +10,40 @@ $('.banner__container').slick({
     autoplaySpeed: 4000,
   });
 
-
-//   $('.').slick({
-//     dots: true,
-//     infinite: false,
-//     speed: 300,
-//     slidesToShow: 4,
-//     slidesToScroll: 4,
-//     responsive: [
-//       {
-//         breakpoint: 1024,
-//         settings: {
-//           slidesToShow: 3,
-//           slidesToScroll: 3,
-//           infinite: true,
-//           dots: true
-//         }
-//       },
-//       {
-//         breakpoint: 600,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 2
-//         }
-//       },
-//       {
-//         breakpoint: 480,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1
-//         }
-//       }
-//       // You can unslick at a given breakpoint now by adding:
-//       // settings: "unslick"
-//       // instead of a settings object
-//     ]
-//   });
+  // Testimonial slider 
+  $('.testimonial__main__container').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 400,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
